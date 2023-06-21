a4bef8b3620db189ed67245538ba55fe2b87f2cb
kevincnguyen <kevincuunguyen@gmail.com>
2023-06-20 21:02:55 -0700

    Debugged incorrect naming bug on CourseInfo in Part 1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
       <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
-  )
+  );
 }
 
 // renders the name of the course
@@ -34,18 +34,18 @@ const Header = props => {
     <h1>
       {props.course}
     </h1>
-  )
+  );
 }
 
 // renders the parts and their number of exercises
 const Content = props => {
   return (
     <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercise} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercise} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercise} />
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
     </div>
-  )
+  );
 }
 
 // each renders the name and number of exercises of one part
@@ -54,16 +54,16 @@ const Part = props => {
     <p>
       {props.part} {props.exercise}
     </p>
-  )
+  );
 }
 
 // renders the total number of exercises
 const Total = props => {
   return (
     <p>
-      Number of exercises {props.parts[0].exercise + props.parts[1].exercise + props.parts[2].exercise}
+      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
     </p>
-  )
+  );
 }
 
 export default App

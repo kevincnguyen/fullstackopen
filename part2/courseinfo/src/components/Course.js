c063b6cdc4872d863853650c2bbe7d552e8ecbd5
kevincnguyen <kevincuunguyen@gmail.com>
2023-06-22 11:11:14 -0700

    Updated dependencies

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -16,31 +16,31 @@ const Header = ({ course }) => {
   
 const Content = ({ parts }) => {
     return (
-    <>
-        {parts.map((part) => <Part part={part} />)}
-    </>
+        <>
+            {parts.map((part) => <Part part={part} />)}
+        </>
     )
 } 
   
 const Part = ({ part }) => {
     return (
-    <p>
-        {part.name} {part.exercises}
-    </p>
+        <p>
+            {part.name} {part.exercises}
+        </p>
     )
 }
 
 const Total = ({ sum }) => {
     return (
-    <p>
-        <strong>Number of exercises {sum}</strong>
-    </p>
+        <p>
+            <strong>Number of exercises {sum}</strong>
+        </p>
     )
 }
 
 const calculateSum = (parts) => {
     const sum = parts.reduce((totalExercises, part) => {
-    return totalExercises + part.exercises
+        return totalExercises + part.exercises
     }, 0)
     return sum
 }

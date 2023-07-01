847754ca9fe35b790b9e85dbf802ffad878b8e5a
kevincnguyen <kevincuunguyen@gmail.com>
2023-07-01 13:17:33 -0700

    Completed Exercises 4.1-4.2

---
+++ part4/blog/app.js
@@ -0,0 +1,17 @@
+const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect(config.MONGODB_URI);
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app; 
